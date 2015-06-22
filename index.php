f5fa1ce9ded557193cd5dfae58b72710da334780
Abdelkader <punshermann@gmail.com>
2015-06-22 03:41:57 +0000

    Upload

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>NairaTip </title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+
+<link rel="stylesheet" href="bootstrap2/css/bootstrap.min.css" />
+<link rel="stylesheet" href="bootstrap2/css/bootstrap-theme.min.css" />
+
+<script type="text/javascript" src="bootstrap2/js/jquery.min.js" ></script>
+<script type="text/javascript" src="bootstrap2/js/bootstrap.min.js" ></script>
+
+<link rel="stylesheet" href="bootstrap2/css/base.css" />
+</head>
+<body >
+
+
+
+
+<div class="container-fluid">
+
+	<div class="row">
+
+		<div class="hero-unit panel-footer" align="center">
+
+		<nav class="visible-xs">
+		  <div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			  <ul class="list-inline">
+				<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+				<li><a href="#">Start Tipping</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Contact</a></li>
+				<li class="btn btn-default pull-right" ><a style="color:#000" href="#">Login</a></li>
+			  </ul>
+
+			</div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+
+			<div class="dist-unit">
+			 
+			 <h1>NairaTip</h1>
+				
+			  <p>Social Payments For A Social World</p>
+
+			</div>
+
+		</div>
+		
+	</div>	
+
+	<div class="row row-bar">
+
+		<div class="col-sm-12 panel-footer hidden-xs" >
+		 
+		 <ul class="nav navbar-nav" >
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Guide</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+		</ul>
+
+		</div>
+
+	</div>
+</div>
+</br></br>
+	
+<div class="container" >
+	
+	<div class="intro col-xs-offset-1" align="center">
+			
+			<h1 class="text-info ">Why NairaTip ?</h1>
+		
+		  <blockquote class="text-muted text-danger" style="font-weight:bold; font-size: 22px;">
+		  NairaTip is a micropayment software that 
+		  enables people around the globe to send
+		  and receive money on social media networks.
+		  </blockquote>
+		<button class="btn btn-success centered">Login</button>
+	</div>
+</br></br></br></br></br>
+	
+<div class="bs-medias col-xs-offset-2" data-example-id="default-media">
+
+    <div class="media">
+      <div class="media-body">
+		<div class="media col-xs-12">
+          <div class="media-left">
+            <a href="#">
+              <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNGUxODMyZGI0ZSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE0ZTE4MzJkYjRlIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNC41IiB5PSIzNi41Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true" style="width: 96px; height: 96px;">
+            </a>
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Repaying small debts</h4>
+           Pay fast and easy your debts wth NGN or Bitcoin !
+          </div>
+        </div>
+      </div>
+    </div>   
+<hr class="text-limit">
+	<div class="media">
+      <div class="media-body">
+		<div class="media col-xs-10">
+          <div class="media-left">
+            <a href="#">
+              <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNGUxODMyZGI0ZSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE0ZTE4MzJkYjRlIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNC41IiB5PSIzNi41Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true" style="width: 96px; height: 96px;">
+            </a>
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Accepting payment on the internet .</h4>
+           You can accept now payment and making your small business more profitable with our low fees system !
+          </div>
+        </div>
+      </div>
+    </div>	
+<hr class="text-limit">	
+	<div class="media">
+      <div class="media-body">
+		<div class="media col-xs-10">
+          <div class="media-left">
+            <a href="#">
+              <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNGUxODMyZGI0ZSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE0ZTE4MzJkYjRlIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNC41IiB5PSIzNi41Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true" style="width: 96px; height: 96px;">
+            </a>
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Charitable donations.</h4>
+          You can start tip charity now .
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  
+
+
+</br></br></br>
+
+</div>
+	
+<div class="bs-end panel-footer" align="center">
+			<div class="dist-unit">
+			
+			 <h3> First Nigerian Tipping plateform !</h3>
+				
+				
+			  <p>
+			  <hr class="col-md-2 col-md-offset-3 col-xs-1 col-xs-offset-3">
+			  <b class="col-md-2 col-xs-3" style="color:#FE8500;"><br />Send a feedback </b>
+			  <hr class="col-md-2 col-xs-1">
+			  </p>
+
+			</div>
+			<br /><br />
+</div>	
+
+
+
+	<div class="bs-footer panel-footer" align="center">
+			<div class="dist-unit">
+			 
+			 <h3>NairaTip</h3>
+				
+			  <p>All right reserved © 2015</p>
+
+			</div>
+</div>		
+	
+</body>
+</html>
